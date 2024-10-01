7a05f4592b65aa78630b0e2facb4f364f03cb0a9
Adam Ellouze <elzadam11@gmail.com>
2024-10-01 20:09:43 +0100

    Delete build directory

--- build/CMakeFiles/game.dir/Game/game.c.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/game.dir/Game/game.c.o: \
- /home/fbdev/dev/Stellar-Engine/Game/game.c /usr/include/stdc-predef.h \
- /home/fbdev/dev/Stellar-Engine/Game/../src/engine.h
--- build/CMakeFiles/stellarengine.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for stellarengine.
-# This may be replaced when dependencies are built.
