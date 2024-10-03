57c4392afb80979dc2d750f9229c2f9e4fc75937
Adam Ellouze <elzadam11@gmail.com>
2024-10-03 18:21:35 +0100

    Update engine.h

--- src/engine.h
+++ src/engine.h
@@ -1,10 +1,6 @@
 #ifndef ENGINE_H
 #define ENGINE_H
 
-typedef struct {
-    float vertices[8][3];  // Coordonnées des sommets (8 sommets pour un cube par exemple)
-} Model;
-
 void engineInit();  // Initialiser le moteur et la fenêtre
 void engineLoadModel(const char* modelPath, Model* model);  // Charger un modèle 3D
 void engineRenderScene(Model* model);  // Rendre la scène avec le modèle
